3abea55e4ee27296c535178de00a37d4c5775961
johan.karlsson.malmo <johan.karlsson.malmo@6408a9d0-90c7-11dd-b633-d39c22b7b77f>
2008-10-23 18:58:57 +0000

    Added the possibility to set the InstallListener.

--- src/net/sf/microinstall/SmsInstaller.java
+++ src/net/sf/microinstall/SmsInstaller.java
@@ -28,80 +28,106 @@ import javax.wireless.messaging.TextMessage;
  * @author Johan Karlsson (johan.karlsson@jayway.se)
  */
 public class SmsInstaller {
-	
-	private static final int SMS_LENGTH = 158; //two chars (0x0A) are added below
-	
-	private MessageConnection messageConnection;
 
-	/**
-	 * Sends an installation SMS to the specified telephone number and with the
-	 * specified installation text.
-	 * 
-	 * @param telephoneNo
-	 *            the telephone number to send the installation SMS to.
-	 * @param installationText
-	 *            the text that precedes the installation link.
-	 * @param installationURL
-	 *            the URL where to find the MIDlet. This could either be a link
-	 *            to an JAD file or an JAR file, where the JAD file is the
-	 *            preferred way to do it.
-	 * 
-	 */
-	public void install(final String telephoneNo, String installationText,
-			String installationURL) throws IllegalArgumentException {
-		if (installationURL == null) {
-			throw new IllegalArgumentException(
-					"The installationURL must not be null.");
-		}
-		
-		if ((installationURL.length()+installationText.length()) > SMS_LENGTH) {
-			throw new IllegalArgumentException(
-					"SMS content exceeds "+SMS_LENGTH+" characters.");
-		}
-		
-		
-		
-		StringBuffer messageBuffer = new StringBuffer(200);
-		if (installationText != null) {
-			messageBuffer.append(installationText);
-		} else {
-			messageBuffer.append("Please select the following link.");
-		}
+    private static final int SMS_LENGTH = 158; // two chars (0x0A) are added
+    // below
 
-		messageBuffer.append(new char[] { 0x0A });
-		messageBuffer.append(installationURL);
-		messageBuffer.append(new char[] { 0x0A });
-		final String messageString = messageBuffer.toString();
+    private InstallListener installListener;
 
-		new Thread(new Runnable() {
-			public void run() {
-				sendMessage(telephoneNo, messageString);
-			}
+    private MessageConnection messageConnection;
 
-		}).start();
+    /**
+     * Set the install listener.
+     * 
+     * @param listener
+     *            the listener. Set it to <code>null</code> to remove the
+     *            listener.
+     */
+    public void setInstallListener(InstallListener listener) {
+        this.installListener = listener;
+    }
 
-	}
+    /**
+     * Sends an installation SMS to the specified telephone number and with the
+     * specified installation text.
+     * 
+     * @param telephoneNo
+     *            the telephone number to send the installation SMS to.
+     * @param installationText
+     *            the text that precedes the installation link.
+     * @param installationURL
+     *            the URL where to find the MIDlet. This could either be a link
+     *            to an JAD file or an JAR file, where the JAD file is the
+     *            preferred way to do it.
+     * 
+     */
+    public void install(final String telephoneNo, String installationText,
+            String installationURL) throws IllegalArgumentException {
+        if (installationURL == null) {
+            throw new IllegalArgumentException(
+                    "The installationURL must not be null.");
+        }
 
-	/**
-	 * Send the actual message.
-	 * 
-	 * @param phoneNo
-	 *            the phone number to send the message to.
-	 * @param messageString
-	 *            the message string that is contained in the message.
-	 */
-	private void sendMessage(String phoneNo, String messageString) {
-		try {
-			messageConnection = (MessageConnection) Connector.open("sms://"
-					+ phoneNo);
-			final TextMessage textMessage = (TextMessage) messageConnection
-					.newMessage(MessageConnection.TEXT_MESSAGE);
+        int installationTextLength = 0;
+        if (installationText != null) {
+            installationText.length();
+        }
 
-			textMessage.setPayloadText(messageString);
-			messageConnection.send(textMessage);
+        if (installationTextLength + (installationURL.length()) > SMS_LENGTH) {
+            throw new IllegalArgumentException("SMS content exceeds "
+                    + SMS_LENGTH + " characters.");
+        }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        StringBuffer messageBuffer = new StringBuffer(200);
+        if (installationText != null) {
+            messageBuffer.append(installationText);
+        } else {
+            messageBuffer.append("Please select the following link.");
+        }
+
+        messageBuffer.append(new char[] { 0x0A });
+        messageBuffer.append(installationURL);
+        messageBuffer.append(new char[] { 0x0A });
+        final String messageString = messageBuffer.toString();
+
+        new Thread(new Runnable() {
+            public void run() {
+                sendMessage(telephoneNo, messageString);
+            }
+
+        }).start();
+
+    }
+
+    /**
+     * Send the actual message. If there is an <code>InstallListener</code>
+     * registered it will be notified when the message has been sent.
+     * 
+     * @param phoneNo
+     *            the phone number to send the message to.
+     * @param messageString
+     *            the message string that is contained in the message.
+     */
+    private void sendMessage(String phoneNo, String messageString) {
+        try {
+            messageConnection = (MessageConnection) Connector.open("sms://"
+                    + phoneNo);
+            final TextMessage textMessage = (TextMessage) messageConnection
+                    .newMessage(MessageConnection.TEXT_MESSAGE);
+
+            textMessage.setPayloadText(messageString);
+            messageConnection.send(textMessage);
+
+            if (installListener != null) {
+                installListener.installationFinished();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (installListener != null) {
+                installListener.installationFailed(
+                        "Failed to send the installation SMS", e);
+            }
+        }
+    }
 }
